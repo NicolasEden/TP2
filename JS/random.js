fa8cde6e07e93847a596d425fb88e9e056e27197
NefiteTifall <nefitetifall@gmail.com>
2019-11-12 09:00:52 +0100

    Work 12/11

---
+++ JS/random.js
@@ -0,0 +1,45 @@
+function info() {
+    alert('COUCOU !')
+}
+function info1() {
+    document.getElementById("hover1").style.display = "block";
+}
+function rinfo1(){
+    document.getElementById("hover1").style.display = "none";
+}
+function info2() {
+    document.getElementById("hover2").style.display = "block";
+}
+function rinfo2(){
+    document.getElementById("hover2").style.display = "none";
+}
+function info3() {
+    document.getElementById("hover3").style.display = "block";
+}
+function rinfo3(){
+    document.getElementById("hover3").style.display = "none";
+}
+function info4() {
+    document.getElementById("hover4").style.display = "block";
+}
+function rinfo4(){
+    document.getElementById("hover4").style.display = "none";
+}
+function info5() {
+    document.getElementById("hover5").style.display = "block";
+}
+function rinfo5(){
+    document.getElementById("hover5").style.display = "none";
+}
+function info6() {
+    document.getElementById("hover6").style.display = "block";
+}
+function rinfo6(){
+    document.getElementById("hover6").style.display = "none";
+}
+function info7() {
+    document.getElementById("hover7").style.display = "block";
+}
+function rinfo7(){
+    document.getElementById("hover7").style.display = "none";
+}
